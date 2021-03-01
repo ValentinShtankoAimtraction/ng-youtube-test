c533a72c468d2f255c48ed5bc2080d0465da2c2f
ValentinShtankoAimtraction <valentin.shtanko@aimtraction.com>
2021-03-01 16:28:25 +0200

    Import data grid and grid toolbar components to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {COMPONENTS} from './components';
 import {CONTAINERS} from './containers';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CONTAINERS
+    CONTAINERS,
+    COMPONENTS
   ],
   imports: [
     BrowserModule
@@ -15,4 +17,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/components/index.ts
@@ -0,0 +1,7 @@
+import {YtDataGridComponent} from './yt-data-grid/yt-data-grid.component';
+import {YtGridToolbarComponent} from './yt-grid-toolbar/yt-grid-toolbar.component';
+
+export const COMPONENTS = [
+  YtDataGridComponent,
+  YtGridToolbarComponent
+];
