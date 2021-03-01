a83116d15cd891c0b95f7a22fa77552b78696525
ValentinShtankoAimtraction <valentin.shtanko@aimtraction.com>
2021-03-01 19:09:35 +0200

    Implemented items data as observable.

--- src/app/services/yt-data.service.ts
+++ src/app/services/yt-data.service.ts
@@ -1,18 +1,25 @@
 import {HttpClient} from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import {IYtRequestOptions} from 'src/app/models';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, Observable, Subject} from 'rxjs';
+import {IYtItem, IYtRequestOptions, IYtResponse} from 'src/app/models';
 import {environment} from 'src/environments/environment';
 
 @Injectable()
 export class YtDataService {
+  public ytItems$: Observable<IYtItem[]>;
+  private _ytItemsSubject: Subject<IYtItem[]>;
   private _requestOptions: IYtRequestOptions;
+
   constructor(private _http: HttpClient) {
     this._requestOptions = {
       maxResults: '50',
       type: 'video',
       part: 'snippet',
       q: 'john'
-    }
+    };
+
+    this._ytItemsSubject = new BehaviorSubject<IYtItem[]>([]);
+    this.ytItems$ = this._ytItemsSubject.asObservable();
   }
 
   fetchData() {
@@ -21,6 +28,10 @@ export class YtDataService {
         key: environment.api_key,
         ...this._requestOptions,
       }
-    }).toPromise()
+    }).toPromise().then(
+      (response: IYtResponse) => {
+        this._ytItemsSubject.next(response.items);
+      }
+    )
   };
 }
