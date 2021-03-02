736e511b6ea3cda92b659b63944ec4db0c2a5e15
ValentinShtankoAimtraction <valentin.shtanko@aimtraction.com>
2021-03-02 16:05:55 +0200

    [STORE] Created video reducer and state

---
+++ src/app/store/reducers/video.reducer.ts
@@ -0,0 +1,51 @@
+import {createEntityAdapter, EntityAdapter, EntityState} from '@ngrx/entity';
+import {IDtItem} from 'src/app/models';
+import {VideoActions, VideoActionTypes} from 'src/app/store/actions/video.actions';
+
+export interface State extends EntityState<IDtItem> {
+  loaded: boolean;
+  loading: boolean;
+  error: any;
+  selectedVideos: string[];
+}
+
+export const adapter: EntityAdapter<IDtItem> = createEntityAdapter<IDtItem>();
+
+export const initialState: State = adapter.getInitialState({
+  loaded: false,
+  loading: false,
+  error: null,
+  selectedVideos: []
+});
+
+export function reducer(state = initialState, action: VideoActions): State {
+  switch (action.type) {
+    case VideoActionTypes.videoFetchItems:
+      return {
+        ...state,
+        loading: true
+      };
+    case VideoActionTypes.videoFetchItemsSuccess:
+      return adapter.addMany(action.payload, {
+        ...state,
+        loading: false,
+        loaded: true
+      });
+    case VideoActionTypes.videoSelectItem:
+      return {
+        ...state,
+        selectedVideos: [...state.selectedVideos, action.payload]
+      };
+    case VideoActionTypes.videoError:
+      return {
+        ...state,
+        loaded: false,
+        loading: true,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+}
+
+export const videoEntitySelectors = adapter.getSelectors();
