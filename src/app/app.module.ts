e0364450a6161a26d1014308e5e80b0cabef9069
ValentinShtankoAimtraction <valentin.shtanko@aimtraction.com>
2021-03-02 15:30:40 +0200

    Installed ngrx store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import {ModuleRegistry} from '@ag-grid-community/core';
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from 'src/environments/environment';
 
 import {AppComponent} from './app.component';
 import {COMPONENTS, DtImageColumnComponent} from './components';
@@ -24,10 +26,14 @@ ModuleRegistry.registerModules([
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
     MaterialModule,
     AgGridModule.withComponents([DtImageColumnComponent]),
-    BrowserAnimationsModule,
   ],
   providers: [YtDataService],
   bootstrap: [AppComponent],
