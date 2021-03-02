e246b2c239e2c36ab073efbb0e3031929cafdcd2
ValentinShtankoAimtraction <valentin.shtanko@aimtraction.com>
2021-03-02 10:00:50 +0200

    Provided items from service to container

--- src/app/containers/yt-grid-container/yt-grid-container.component.ts
+++ src/app/containers/yt-grid-container/yt-grid-container.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {Observable} from 'rxjs';
+import {IDtItem} from 'src/app/models/dt-item';
 import {YtDataService} from 'src/app/services/yt-data.service';
 
 @Component({
@@ -8,7 +10,7 @@ import {YtDataService} from 'src/app/services/yt-data.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class YtGridContainerComponent implements OnInit {
-
+  items$: Observable<IDtItem[]> = this._ytService.ytItems$;
   constructor(private _ytService: YtDataService) { }
 
   ngOnInit(): void {
