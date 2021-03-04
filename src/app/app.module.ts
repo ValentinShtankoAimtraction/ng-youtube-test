cfd98c600f56a64a89f648bf6661fedfaff98e2c
ValentinShtankoAimtraction <valentin.shtanko@aimtraction.com>
2021-03-04 17:46:32 +0200

    Import grid module to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,20 @@
-import {AgGridModule} from '@ag-grid-community/angular';
-import {ClientSideRowModelModule} from '@ag-grid-community/client-side-row-model';
-import {ModuleRegistry} from '@ag-grid-community/core';
-import {MenuModule} from '@ag-grid-enterprise/menu';
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {EffectsModule} from '@ngrx/effects';
+import {StoreModule} from '@ngrx/store';
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {GridModule} from 'src/app/grid/grid.module';
 import {environment} from 'src/environments/environment';
 
 import {AppComponent} from './app.component';
-import {COMPONENTS, DtImageColumnComponent} from './components';
-import {CONTAINERS} from './containers';
-import {MaterialModule} from './material.module';
 import {YtDataService} from './services/yt-data.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from '@ngrx/store';
 import * as fromStore from './store';
-ModuleRegistry.registerModules([
-  ClientSideRowModelModule,
-  MenuModule
-]);
 
 @NgModule({
   declarations: [
     AppComponent,
-    CONTAINERS,
-    COMPONENTS
   ],
   imports: [
     BrowserModule,
@@ -34,12 +22,12 @@ ModuleRegistry.registerModules([
     HttpClientModule,
     StoreModule.forRoot(fromStore.reducers),
     EffectsModule.forRoot(fromStore.effects),
-    MaterialModule,
-    AgGridModule.withComponents([DtImageColumnComponent]),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
     }),
+
+    GridModule
   ],
   providers: [YtDataService],
   bootstrap: [AppComponent],
--- src/app/services/yt-data.service.spec.ts
+++ src/app/services/yt-data.service.spec.ts
@@ -1,16 +1,40 @@
-import { TestBed } from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { YtDataService } from './yt-data.service';
+import {YtDataService} from './yt-data.service';
 
 describe('YtDataService', () => {
   let service: YtDataService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        YtDataService,
+      ],
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(YtDataService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should fetch items', () => {
+    let mockItems = [];
+    service.fetchVideos().subscribe(
+      (res) => {
+        expect(res).toEqual([])
+      }
+    );
+    const req = httpMock.expectOne('https://www.googleapis.com/youtube/v3/search?key=AIzaSyDnnxSaX4eqqOtDionGFZ5aKq1H0X5tk3c&maxResults=50&type=video&part=snippet&q=ag-grid');
+    expect(req.request.method).toEqual("GET");
+    req.flush(mockItems);
+
+    httpMock.verify();
+  })
 });
