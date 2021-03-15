b52cf7f16cf1f18fa1e600cab729ff810ce93b02
ValentinShtankoAimtraction <valentin.shtanko@aimtraction.com>
2021-03-15 17:28:34 +0200

    Cover by test select header renderer component

--- src/app/shared/components/select-header-renderer/select-header-renderer.component.spec.ts
+++ src/app/shared/components/select-header-renderer/select-header-renderer.component.spec.ts
@@ -1,3 +1,4 @@
+import {GridApi, IHeaderParams} from '@ag-grid-community/core';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 
@@ -22,6 +23,25 @@ describe('SelectHeaderRendererComponent', () => {
   });
 
   it('should create', () => {
+    component.afterGuiAttached();
     expect(component).toBeTruthy();
   });
+
+  it('should be return disable refresh state', () => {
+    expect(component.refresh(<IHeaderParams>{})).toBeFalsy();
+  });
+
+  it('should select emit selectAll event', () => {
+    component.gridApi = <GridApi>{selectAll: () => {}};
+    let spy = spyOn(component.gridApi, 'selectAll');
+    component.selectAll();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should select emit unselectAll event', () => {
+    component.gridApi = <GridApi>{deselectAll: () => {}};
+    let spy = spyOn(component.gridApi, 'deselectAll');
+    component.unselectAll();
+    expect(spy).toHaveBeenCalled();
+  })
 });
