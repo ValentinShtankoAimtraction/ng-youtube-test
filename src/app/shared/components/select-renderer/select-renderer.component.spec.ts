23724d7f1025a9feca1decea7120d50e45117d2e
ValentinShtankoAimtraction <valentin.shtanko@aimtraction.com>
2021-03-15 16:58:48 +0200

    Cover by test select renderer component

--- src/app/shared/components/select-renderer/select-renderer.component.spec.ts
+++ src/app/shared/components/select-renderer/select-renderer.component.spec.ts
@@ -1,5 +1,5 @@
 import {AgGridModule} from '@ag-grid-community/angular';
-import {RowNode} from '@ag-grid-community/core';
+import {ICellRendererParams, RowNode} from '@ag-grid-community/core';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 
@@ -8,7 +8,6 @@ import {SelectRendererComponent} from './select-renderer.component';
 describe('SelectRendererComponent', () => {
   let component: SelectRendererComponent;
   let fixture: ComponentFixture<SelectRendererComponent>;
-  let node: RowNode;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -28,4 +27,25 @@ describe('SelectRendererComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should set checked status', () => {
+    let node = {isSelected: () => true};
+    component.agInit({node});
+    expect(component.checked).toBeTruthy();
+  });
+
+  it('should refresh params', () => {
+    let node = {isSelected: () => true};
+    component.agInit({node});
+    let refreshNode = {isSelected: () => false};
+    component.refresh(<ICellRendererParams>{node: refreshNode} );
+    expect(component.checked).toBeFalsy();
+  });
+
+  it('should change selection status', () => {
+    component.node = {setSelected: (newValue: boolean, clearSelection?: boolean, suppressFinishActions?: boolean) => {}} as RowNode;
+    let spy = spyOn(component.node, 'setSelected');
+    component.changeSelection({checked: true});
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith(true);
+  })
 });
